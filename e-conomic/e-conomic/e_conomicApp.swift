74d32c93f5a4bf2b8a333c64e8611739bd6333e7
“Mdmitriy119E” <dumitru.manea@endava.com>
2024-07-23 16:11:25 +0300

    feat: project init

---
+++ e-conomic/e-conomic/e_conomicApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_conomicApp.swift
+//  e-conomic
+//
+//  Created by Dumitru Manea on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct e_conomicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
