04000db51ff583dff041fe18e2d962b78aa20198
“Mdmitriy119” <dumitrumanea119@gmail.com>
2024-07-23 22:12:29 +0300

    refactor: expenses data manager

--- e-conomic/e-conomic/Shared/CoreData/ExpensesDataManager.swift
+++ e-conomic/e-conomic/Shared/CoreData/ExpensesDataManager.swift
@@ -7,17 +7,73 @@
 
 import CoreData
 
-final class ExpensesDataManager {
+protocol ExpensesDataManagerServicing {
+    func addExpense(title: String,
+                    details: String,
+                    total: Double,
+                    currency: String,
+                    date: Date,
+                    photo: NSData?)
+    func delete(expense: Expense)
+}
+
+final class ExpensesDataManager: ExpensesDataManagerServicing {
+    // MARK: - Private properties
+    private let containerName = "ExpensesDataModel"
     private let persistentContainer: NSPersistentContainer
+    private var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
     
+    // MARK: - Public properties
     static let shared = ExpensesDataManager()
+    let fetchResultsController: NSFetchedResultsController<Expense>
     
+    // MARK: - Initializer
     private init() {
-        persistentContainer = NSPersistentContainer(name: "ExpensesDataModel")
+        persistentContainer = NSPersistentContainer(name: containerName)
         persistentContainer.loadPersistentStores { (storeDescription, error) in
             if let error {
-                fatalError("Error: \(error.localizedDescription)")
+                fatalError(error.localizedDescription)
             }
         }
+        let fetchRequest: NSFetchRequest<Expense> = Expense.fetchRequest() as! NSFetchRequest<Expense>
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+    }
+}
+
+// MARK: - Private methods
+private extension ExpensesDataManager {
+    func saveIfNeeded() {
+        if context.hasChanges {
+           try? context.save()
+        }
+    }
+}
+
+// MARK: - Public methods
+extension ExpensesDataManager {
+    func addExpense(title: String,
+                    details: String,
+                    total: Double,
+                    currency: String,
+                    date: Date,
+                    photo: NSData?) {
+        let _ = Expense(
+            context: context,
+            title: title,
+            details: details,
+            total: total,
+            currency: currency,
+            date: date,
+            photo: photo)
+        saveIfNeeded()
+    }
+    
+    func delete(expense: Expense) {
+        context.delete(expense)
+        saveIfNeeded()
     }
 }
